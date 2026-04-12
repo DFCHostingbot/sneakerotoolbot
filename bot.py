130322a35062a943d6dec57b60ebe5be0db22055
DFCHostingbot <dani_alberti_@outlook.com>
2026-04-12 20:26:47 +0200

    Integrate Flask web server into Discord bot
    Added a Flask web server to prevent Render downtime.

--- bot.py
+++ bot.py
@@ -1,6 +1,16 @@
 import os
 import discord
+from discord.ext import commands
+from flask import Flask
 
+# Nep webserver zodat Render niet klaagt
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "OK"
+
+# Discord bot (helemaal leeg)
 client = discord.Client(intents=discord.Intents.default())
 
 client.run(os.getenv("TOKEN"))
